5b4ac14f5556256e2deaefad0d01e1661137981d
Gustavo Anjos <gustavoanjos42@gmail.com>
2021-01-18 17:38:47 -0400

    Initial Commit

---
+++ ReadMeetSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReadMeetSwiftUI
+//
+//  Created by Gustavo Anjos on 18/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ReadMeetSwiftUI/ReadMeetSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeetSwiftUIApp.swift
+//  ReadMeetSwiftUI
+//
+//  Created by Gustavo Anjos on 18/01/21.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeetSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
